3b587e5576507df89d96ed9476f60bcf91f45928
Neo2057 <neotatsux@gmail.com>
2020-12-03 10:41:20 +0900

    Initial commit

---
+++ src/main/java/neo2057/github/io/git/neomultiworld/NeoMultiWorld.java
@@ -0,0 +1,17 @@
+package neo2057.github.io.git.neomultiworld;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NeoMultiWorld extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
